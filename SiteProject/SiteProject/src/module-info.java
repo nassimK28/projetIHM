c7f0c83f587e9fec9cfdea0c0b831b3a0073a6b1
nassimK28 <153238924+nassimK28@users.noreply.github.com>
2023-12-07 23:31:47 +0100

    Add files via upload

---
+++ SiteProject/SiteProject/src/module-info.java
@@ -0,0 +1,8 @@
+module SiteProject {
+	requires javafx.controls;
+	requires javafx.web;
+	requires javafx.graphics;
+	requires javafx.base;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
